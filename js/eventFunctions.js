4dcc777cae9a584749eeb64c57465f62ea320255
elandini84 <62991576+elandini84@users.noreply.github.com>
2021-05-27 16:19:49 +0200

    Starting commit

---
+++ js/eventFunctions.js
@@ -0,0 +1,9 @@
+// Takes a snapshot from the stream image
+function snapStream() {
+    var img = document.getElementById("stream_img");
+}
+
+// Takes a snapshot from the map image
+function snapMap() {
+    
+}
---
+++ server.py
@@ -0,0 +1,23 @@
+import tornado.ioloop
+import tornado.web
+import json
+import os
+
+games = [...]
+
+class HomeHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('index.html')
+
+path = os.path.join(os.getcwd())
+if __name__ == "__main__":
+    application = tornado.web.Application(
+        [
+            (r'/', HomeHandler),
+            (r'/(.*\..*)', tornado.web.StaticFileHandler, {'path': path})
+        ],
+        template_path=os.path.dirname(__file__),
+        debug=True
+    )
+    application.listen(16001)
+    tornado.ioloop.IOLoop.current().start()
