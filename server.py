46e08adb7789054efcc9ca6d94e76f5189ccb12e
Ettore Landini <ettore.landini@iit.it>
2021-06-09 16:11:15 +0200

    ResourceFinder and cosmetic changes

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from tornado.web import RequestHandler, StaticFileHandler
 from tornado.websocket import WebSocketHandler
-from yarp import Port, Network, Bottle
+from yarp import Port, Network, Bottle, ResourceFinder
 from threading import Lock
 import json
 import os
@@ -10,6 +10,7 @@ from python_code.skelServer import SkelServer
 
 ABSPATH = os.path.dirname(os.path.realpath(__file__))
 NETWORK = None
+RESFINDER = None
 CLICKPORT = None
 CLICKPORTNAME = "/webview/click:o"
 CAMERAPORT = "/freeFloorViewer/floorEnhanced:o"
@@ -22,12 +23,9 @@ class IndexHandler(RequestHandler):
         global NETWORK
         global CAMERAPORT
         global MAPPORT
-        if len(sys.argv) >= 3:
-            contactCamera = NETWORK.queryName(sys.argv[1])
-            contactMap = NETWORK.queryName(sys.argv[2])
-        else:
-            contactCamera = NETWORK.queryName(CAMERAPORT)
-            contactMap = NETWORK.queryName(MAPPORT)
+        global RESFINDER
+        contactCamera = NETWORK.queryName(RESFINDER.find("camera_port").asString() if RESFINDER.check("camera_port") else CAMERAPORT)
+        contactMap = NETWORK.queryName(RESFINDER.find("map_port").asString() if RESFINDER.check("map_port") else MAPPORT)
         self.render(ABSPATH + '{0}static{0}html{0}index.html'.format(os.sep),
                     porta1 = contactCamera.getHost()+":"+str(contactCamera.getPort()),
                     porta2 = contactMap.getHost()+":"+str(contactMap.getPort()))
@@ -59,7 +57,10 @@ if __name__ == "__main__":
 
     NETWORK = Network()
     NETWORK.init()
+    RESFINDER = ResourceFinder()
+    RESFINDER.configure(sys.argv)
     CLICKPORT = Port()
+    CLICKPORTNAME = RESFINDER.find("click_port").asString if RESFINDER.check("click_port") else CLICKPORTNAME
     CLICKPORT.open(CLICKPORTNAME)
     handlersList = [(r'/', IndexHandler),
                     (r"/static/(.*)", StaticFileHandler,{'path':'static'}),
