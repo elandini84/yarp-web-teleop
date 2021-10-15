23a08ef381fbd7f2791a633c4b1fc04f1e07928c
elandini84 <62991576+elandini84@users.noreply.github.com>
2021-10-15 09:13:32 +0200

    Coordinates conversion

--- static/js/eventFunctions.js
+++ static/js/eventFunctions.js
@@ -1,6 +1,15 @@
 let positionX = -1
 let positionY = -1
-var ws = new WebSocket("ws://"+window.location.host+"/ws");
+let drag = false;
+let ws = new WebSocket("ws://"+window.location.host+"/ws");
+
+function convertMousePos(x,y){
+    var camera = $("#camera_img");
+    var conversionX = camera.prop("naturalWidth")/camera.width();
+    var conversionY = camera.prop("naturalHeight")/camera.height();
+    return {"x": Math.round(x*conversionX),
+            "y": Math.round(y*conversionY)};
+}
 
 function updateMousePos() {
     $("#clickXTxt").html(positionX);
@@ -9,39 +18,45 @@ function updateMousePos() {
                "y":positionY};
     ws.send(JSON.stringify(msg));
 }
+
 function manageDrag(e) {
     var camera = $("#camera_img");
     var posX = camera.offset().left
     var posY = camera.offset().top;
-    cursorX = e.pageX - posX;
-    cursorY = e.pageY - posY;
-    diffX = Math.abs(positionX - cursorX);
-    diffY = Math.abs(positionY - cursorY);
+    var cursorX = e.pageX - posX;
+    var cursorY = e.pageY - posY;
+    var cursors = convertMousePos(cursorX,cursorY);
+    var diffX = Math.abs(positionX - cursors.x);
+    var diffY = Math.abs(positionY - cursors.y);
     if (diffX < 5 && diffY < 5) 
     {
         updateMousePos();
     } else 
     {
-        $("#clickXTxt").html(positionX+" to "+cursorX);
-        $("#clickYTxt").html(positionY+" to "+cursorY);
+        $("#clickXTxt").html(positionX+" to "+cursors.x);
+        $("#clickYTxt").html(positionY+" to "+cursors.y);
 
         var msg = {"x-start":positionX,
                    "y-start":positionY,
-                   "x-end":cursorX,
-                   "y-end":cursorY};
+                   "x-end":cursors.x,
+                   "y-end":cursors.y};
         ws.send(JSON.stringify(msg));
     }
 }
+
 function simpleDown(e) {
     var camera = $("#camera_img");
     var posX = camera.offset().left
     var posY = camera.offset().top;
-    positionX = e.pageX - posX;
-    positionY = e.pageY - posY;
+    var fakeX = e.pageX - posX;
+    var fakeY = e.pageY - posY;
+    var position = convertMousePos(fakeX,fakeY);
+    positionX = position.x;
+    positionY = position.y;
     drag = false;
 }
+
 function init() {
-    console.log("merda");
     var camera = $("#camera_img");
     //document.getElementById("image_raw").addEventListener("click", printMousePos);
     //document.getElementById("image_raw").addEventListener("drag", printMousePos);
@@ -50,8 +65,6 @@ function init() {
     //document.getElementById('camera_img').ondragstart = function() { return false; };
     //document.getElementById('camera_img').oncontextmenu = function() { return false; };
 
-    let drag = false;
-
     camera.mousedown((e) => simpleDown(e));
     camera.mousemove(function(){drag = true;});
     camera.mouseup((e) => drag ? manageDrag(e) : updateMousePos());
@@ -59,6 +72,7 @@ function init() {
     //document.getElementById("camera_img").addEventListener('mousemove', (e) => drag = true);
     //document.getElementById("camera_img").addEventListener('mouseup', (e) => drag ? printDragMousePos(e) : updateMousePos());
 }
+
 function showNatSize() {
     var camera = $("#camera_img");
     var map = $("#map_img");
