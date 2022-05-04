76a98169aaa15888186d3099747ab6c123e79651
Ettore Landini <ettore.landini@iit.it>
2022-05-04 16:15:10 +0200

    Changed import for yarp related modules

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from tornado.web import StaticFileHandler
 import tornado.httpserver
-from yarp import Port, Network, Bottle, ResourceFinder
+import yarp
 from threading import Lock
 import sqlite3 as sl
 import json
@@ -57,7 +57,7 @@ def createUsersTable(inputDb):
 
 if __name__ == "__main__":
 
-    RESFINDER = ResourceFinder()
+    RESFINDER = yarp.ResourceFinder()
     RESFINDER.configure(sys.argv)
     createUsersTable(loginDb)
 
@@ -89,11 +89,11 @@ if __name__ == "__main__":
                         (r"/wsb", ButtonsHandler, {"webLock": WEBLOCK,
                                                    "navPort": NAVCLICKPORT})]
     else:
-        NETWORK = Network()
+        NETWORK = yarp.Network()
         NETWORK.init()
-        NAVCLICKPORT = Port()
-        MAPCLICKPORT = Port()
-        HEADCLICKPORT = Port()
+        NAVCLICKPORT = yarp.Port()
+        MAPCLICKPORT = yarp.Port()
+        HEADCLICKPORT = yarp.Port()
 
         NAVCLICKPORTNAME = RESFINDER.find("nav_click_port").asString() if RESFINDER.check("nav_click_port") else NAVCLICKPORTNAME
         MAPCLICKPORTNAME = RESFINDER.find("map_click_port").asString() if RESFINDER.check("map_click_port") else MAPCLICKPORTNAME
