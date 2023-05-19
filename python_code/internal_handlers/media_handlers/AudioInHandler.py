7ebb98d48764d0e26c3cf57069f64e5b87a5a196
Ettore Landini <ettore.landini@iit.it>
2023-05-19 18:20:14 +0200

    Some improvements
    When the acquired data are sent all together, the file saved by
    yarp::dev::AudioPlayerWrapper is equal to the one saved py python wave
    package. If the data are sent through the yarp buffered port as soon as
    they arrive from the web page, the file saved by the yarp device is
    "shrinked". To be investigated

--- python_code/internal_handlers/media_handlers/AudioInHandler.py
+++ python_code/internal_handlers/media_handlers/AudioInHandler.py
@@ -2,47 +2,54 @@ import io
 from tornado.websocket import WebSocketHandler
 import wave
 import json
-import sys
+from sys import byteorder
 from random import randint
 from yarp import Sound
+from numpy import int16, frombuffer
+from datetime import datetime
 
 class AudioInHandler(WebSocketHandler):
 
     def initialize(self,soundPort):
         self._sampleRate = 1
-        self._count = randint(0,10)
         self._data = b''
         self._soundPort = soundPort
+        self._sentData = 0
 
     def on_message(self,message):
-        #print("Got {0} of type {1}".format(message,type(message)))
         if type(message) == str:
             options = json.loads(message)
             if "sampleRate" in options.keys():
                 self._sampleRate = int(options["sampleRate"]/3)
                 self._data = b''
+                self._sampleWidth = 2
             else:
                 if options["goOn"]:
-                    waveToFile = wave.open('/home/elandini/mainData/sound_{0}.wav'.format(self._count),'wb')
+                    now_ = datetime.now()
+                    timeStamp = "{0}_{1}_{2}".format(now_.hour,now_.minute,now_.second)
+                    waveToFile = wave.open('sound_{0}.wav'.format(timeStamp),'wb')
                     waveToFile.setnchannels(1)
                     waveToFile.setframerate(self._sampleRate)
-                    waveToFile.setsampwidth(2)
-
-                    print("\n\nwaveToFile: {0} {1}\n\n".format(waveToFile.getframerate(),waveToFile.getsampwidth()))
-
+                    waveToFile.setsampwidth(self._sampleWidth)
                     waveToFile.writeframes(self._data)
                     waveToFile.close()
+
+                    print("Data saved: {0}\nData send: {1}".format(waveToFile.getnframes(),self._sentData))
+
                     self._data = b''
-                    self._count += 1
         else:
-            #sosso = Sound()
             self._data += message
             tempSound = self._soundPort.prepare()
-            tempSound.resize(len(message),1)
+            tempSound.resize(int(len(message)/self._sampleWidth),1)
             tempSound.setFrequency(self._sampleRate)
-            print("tempSound: {0} {1}".format(tempSound.getFrequency(),tempSound.getBytesPerSample()))
-            i = 0
-            for b in message:
-                tempSound.set(b,i,0)
-            print("Sound ready: {0}".format(tempSound.getSamples()))
+            for i in range(int(len(message)/self._sampleWidth)):
+                ind = i+i*(self._sampleWidth-1)
+                b = int(frombuffer(message[ind:(ind+self._sampleWidth)],int16)[0])
+                try:
+                    tempSound.set(b,i,0)
+                except Exception as e:
+                    print("{0}-{1} = {2} ({3} - {4})".format(ind,ind+self._sampleWidth,b,type(b),byteorder))
+                    print("{0} - {1}".format(type(message[i]), message[ind:(ind+self._sampleWidth)]))
+                    print(e)
             self._soundPort.write()
+            self._sentData += int(len(message)/self._sampleWidth)
